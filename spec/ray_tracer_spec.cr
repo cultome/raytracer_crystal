057bb04d1e3d8aec42540cc80ae0533965d98eb1
Carlos Soria <csoria@cultome.io>
2021-10-17 08:25:42 -0500

    Add Vector and Point classes

--- spec/ray_tracer_spec.cr
+++ spec/ray_tracer_spec.cr
@@ -1,9 +1,39 @@
 require "./spec_helper"
 
-describe RayTracer do
-  # TODO: Write tests
+include RayTracer
 
-  it "works" do
-    false.should eq(true)
+describe RayTracer do
+  it "A tuple with w=1.0 is a point" do
+    a = tuple(4.3, -4.2, 3.1, 1.0)
+    a.x.should eq 4.3
+    a.y.should eq -4.2
+    a.z.should eq 3.1
+    a.w.should eq 1.0
+    a.should be_a Point
+    a.should_not be_a Vector
+  end
+
+  it "A tuple with w=0 is a vector" do
+    a = tuple(4.3, -4.2, 3.1, 0.0)
+    a.x.should eq 4.3
+    a.y.should eq -4.2
+    a.z.should eq 3.1
+    a.w.should eq 0.0
+    a.should_not be_a Point
+    a.should be_a Vector
+  end
+
+  it "point() creates tuples with w=1" do
+    p1 = point(4, -4, 3)
+    p2 = tuple(4, -4, 3, 1)
+
+    p1.should eq p2
+  end
+
+  it "vector() creates tuples with w=0" do
+    v1 = vector(4, -4, 3)
+    v2 = tuple(4, -4, 3, 0)
+
+    v1.should eq v2
   end
 end
--- src/ray_tracer.cr
+++ src/ray_tracer.cr
@@ -1,6 +1,37 @@
-# TODO: Write documentation for `RayTracer`
 module RayTracer
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  alias Numeric = Float64 | Int32
+
+  struct Point
+    property x, y, z, w
+
+    def initialize(@x : Numeric, @y : Numeric, @z : Numeric)
+      @w = 1
+    end
+  end
+
+  struct Vector
+    property x, y, z, w
+
+    def initialize(@x : Numeric, @y : Numeric, @z : Numeric)
+      @w = 0
+    end
+  end
+
+  def point(x, y, z)
+    Point.new(x, y, z)
+  end
+
+  def vector(x, y, z)
+    Vector.new(x, y, z)
+  end
+
+  def tuple(x, y, z, w)
+    if w == 0
+      vector(x, y, z)
+    else
+      point(x, y, z)
+    end
+  end
 end
