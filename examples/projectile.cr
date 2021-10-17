8b01182e050fb02fb101b2aaa1135bb0bc59fb48
Carlos Soria <csoria@cultome.io>
2021-10-17 09:49:27 -0500

    Add projectile example

---
+++ examples/projectile.cr
@@ -0,0 +1,45 @@
+require "../src/ray_tracer"
+
+include RayTracer
+
+def tick(env, proj)
+  position = proj.position + proj.velocity
+  velocity = proj.velocity + env.gravity + env.wind
+
+  Projectile.new(position, velocity)
+end
+
+struct Environment
+  property gravity, wind
+
+  def initialize(@gravity : RayTracer::Tuple, @wind : RayTracer::Tuple)
+  end
+end
+
+struct Projectile
+  property position, velocity
+
+  def initialize(@position : RayTracer::Tuple, @velocity : RayTracer::Tuple)
+  end
+
+  def y
+    position.y
+  end
+
+  def x
+    position.x
+  end
+end
+
+# velocity is normalized to 1 unit/tick.
+p = Projectile.new(point(0, 1, 0), vector(1, 1, 0).normalize)
+# gravity -0.1 unit/tick, and wind is -0.01 unit/tick.
+e = Environment.new(vector(0, -0.1, 0), vector(-0.01, 0, 0))
+
+loop do
+  puts "(#{p.x}, #{p.y})"
+
+  break if p.y <= 0
+
+  p = tick(e, p)
+end
